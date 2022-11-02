64384bd276a253a985f4a6e9d07fa2d3c9c1f872
CodeMat <codemat.biz@gmail.com>
2022-11-02 02:47:27 +0100

    updated

--- components/Overview.js
+++ components/Overview.js
@@ -5,12 +5,14 @@ function Overview() {
     <Box px='8' py='12' maxW='3xl' mx='auto'>
       <Heading textAlign='center'>OVERVIEW</Heading>
       <Text mt='6' letterSpacing='1px' fontSize='lg'>
-        Ada Nomeh Unateze is Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Ipsam provident blanditiis asperiores consectetur, illo eius
-        consequatur in ipsa fugiat ratione, quae esse sunt culpa optio amet
-        aperiam tempore maiores quaerat tempora nam? Cumque cupiditate
-        reiciendis tenetur, facere vero consequuntur fugiat deserunt sit
-        reprehenderit, nobis aperiam placeat. Eius molestias recusandae debitis!
+        Nomeh Unateze Beauty Pageant is an annual pageant show that is aimed at
+        showcasing the community, its talents and the positive attributes of a
+        typical Nomeh girl.
+      </Text>
+
+      <Text mt='4' letterSpacing='1px' fontSize='lg'>
+       The pageantry is aimed at promoting peace, unity and
+        progress in the community of Nomeh Unateze.
       </Text>
     </Box>
   );
--- pages/about.js
+++ pages/about.js
@@ -10,17 +10,22 @@ function About() {
     <Box>
       <BannerCard title={title} url={url} />
 
-      <Box px='8' pt='' pb='40' maxW='3xl' mx='auto'>
+      <Box px='8' pt='' pb='40' maxW='3xl' mx='auto' fontSize='lg'>
         {/* <Heading textAlign='center'>ABOUT US</Heading> */}
-        <Text mt='12'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora cum
-          distinctio, fugit illo pariatur, animi facilis provident magni ad est
-          id debitis! Fugiat iste fugit suscipit laboriosam sapiente inventore
-          numquam, rerum eos eaque, esse est perferendis nesciunt unde ipsa
-          expedita sit odio error, accusamus cumque id. Aspernatur autem
-          sapiente quas doloribus in nostrum nesciunt minima debitis laborum
-          iure, dolorum provident explicabo cupiditate tempore facilis illo ex
-          sunt ea? Ipsum, itaque.
+        <Text mt='20'>
+          Nomeh Unateze Beauty Pageant is an annual pageant show that is aimed
+          at showcasing the community, its talents and the positive attributes
+          of a typical Nomeh girl.
+        </Text>
+
+        <Text mt='4'>
+          It is a stepping-stone for any maiden of Nomeh extract who has the dream of
+          reaching a greater height in the pageant world.
+        </Text>
+
+        <Text mt='4'>
+          Also, it is a program which main objective is to bring peace, unity and
+          progress to the people of Nomeh Unateze.
         </Text>
       </Box>
     </Box>
